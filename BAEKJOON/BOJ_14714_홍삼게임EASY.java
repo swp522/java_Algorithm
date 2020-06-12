52b4b2d152f2e5b66e0f19ba8602a797cc132193
swpark <swp522@naver.com>
2020-06-12 19:06:28 +0900

    [BAEKJOON] Uploaded BOJ_14714_홍삼게임EASY. Finished not yet.

---
+++ BAEKJOON/BOJ_14714_홍삼게임EASY.java
@@ -0,0 +1,40 @@
+// 2020_06_09_화_39회차_과제2_홍삼게임EASY
+
+/*
+ * 1. 게임을 최대한 빨리 끝내기 위한 최소 지목 횟수 (끌낼 수 없다면 Evil Galazy 출력)
+ *
+ * 2. 2 ≤ N ≤ 500,   1 ≤ A, B ≤ N, A ≠ B  ,  1 ≤ DA, DB ≤ N − 1
+ *
+ * 3. 구현인데 어려워보인다
+ *
+ * 4. 아직 미완성
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_14714_홍삼게임EASY {
+
+    static int n, a, b, da, db;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        da = Integer.parseInt(st.nextToken());
+        db = Integer.parseInt(st.nextToken());
+
+
+
+
+    }
+
+    public static void solve(){
+
+    }
+}

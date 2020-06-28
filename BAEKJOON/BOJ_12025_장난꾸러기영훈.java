d5ecea6cdcb5e53e6b088473049d1b0bcb2db4de
swpark <swp522@naver.com>
2020-06-28 23:53:07 +0900

    [BAEKJOON] Uploaded BOJ_12025_장난꾸리기영훈. Finished not yet.

---
+++ BAEKJOON/BOJ_12025_장난꾸러기영훈.java
@@ -0,0 +1,19 @@
+// 2020_06_30_화_44회차_과제1_장난꾸러기 영훈
+
+/*
+ * 1. 원래 비밀번호 구하기 (k번쨰 비밀번호 없으면 -1 출력)
+ *
+ * 2. 비밀번호 길이 <= 60, 첫 숫자 0 가능
+ *
+ * 3.
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_12025_장난꾸러기영훈 {
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}

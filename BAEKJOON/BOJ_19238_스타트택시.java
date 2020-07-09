1e7932d813c0fc49a078ef5ce23acce9a7544c00
swpark <swp522@naver.com>
2020-07-09 15:39:00 +0900

    [BAEKJOON] Re-Uploaded BOJ_19238_스타트택시. Finished not yet.

--- BAEKJOON/BOJ_19238_스타트택시.java
+++ BAEKJOON/BOJ_19238_스타트택시.java
@@ -1,12 +1,11 @@
-// 2020_06_19_금_42회차_문풀1_스타트 택시
+// 2020_07_19_금_42회차_문풀1_스타트 택시
 
 /*
  * 1. 남은 연료양 구하기 (연료 바닥나면 이동 못하면 -1 출력)
  *
  * 2. 2 ≤ N ≤ 20,  1 ≤ M ≤ N2,  1 ≤ 초기 연료 ≤ 500,000
  *
- * 3.
- *
+ * 3. 미완성
  *
  *
  */
@@ -14,34 +13,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_19238_스타트택시 {
 
-    static int n, m, fuel, startX, startY, destX, destY, destNum;
-    static int[][] map, dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static boolean[][] visited1, visited2;
-    static ArrayList<Node> customerList;
-    static ArrayList<Node> destinationList;
-    static Queue<Node> q1, q2;
-    static boolean flag;
+    static int[][] map, person;
+    static boolean[][] visited;
+    static int n, m, answer, fuel, startX, startY;
+    static int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-    static class Node implements Comparable<Node> {
+    static class Person implements Comparable<Person> {
         int x, y, num;
 
-        public Node(int x, int y, int num) {
+        public Person(int x, int y, int num) {
             this.x = x;
             this.y = y;
             this.num = num;
         }
 
         @Override
-        public int compareTo(Node o) {
+        public int compareTo(Person o) {
+            if (this.num == o.num) {
+                if (person[this.num][1] == person[o.num][1]) {
+                    return person[this.num][2] - person[o.num][2];
+                }
+                return person[this.num][1] - person[o.num][1];
+            }
             return this.num - o.num;
         }
+    }
+
+    static class Car {
+        int x, y, fuel, count;
+
+        public Car(int x, int y, int fuel, int count) {
+            super();
+            this.x = x;
+            this.y = y;
+            this.fuel = fuel;
+            this.count = count;
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
@@ -62,108 +74,37 @@ public class BOJ_19238_스타트택시 {
         startX = Integer.parseInt(st.nextToken());
         startY = Integer.parseInt(st.nextToken());
 
-
-        customerList = new ArrayList<>();
-        destinationList = new ArrayList<>();
+        person = new int[m][5];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
-            int c = Integer.parseInt(st.nextToken()) - 1;
-            int d = Integer.parseInt(st.nextToken()) - 1;
-            map[a][b] = 2;
-            customerList.add(new Node(a, b, i));
-            destinationList.add(new Node(c, d, i));
+            for (int j = 1; j < 5; j++) {
+                person[i][j] = Integer.parseInt(st.nextToken()) - 1;
+            }
         }
 
-        int round = m;
-        int nowX = startX;
-        int nowY = startY;
-        while (round != 0) {
+        for (int t = 0; t < m; t++) {
+            PriorityQueue<Person> pq = new PriorityQueue<>();
+            visited = new boolean[n][n];
+            Queue<Car> q = new LinkedList<>();
+            visited[startX][startY] = true;
+            q.add(new Car(startX, startY, fuel, 0));
 
-            flag = false;
-            findTheNearestMan(nowX, nowY);
-            goDestination(destX, destY, destNum);
-        }
+            while (!q.isEmpty()) {
+                Car car = q.poll();
 
-        if (flag = true) {
-            System.out.println(fuel);
-        } else {
-            System.out.println(-1);
-        }
-    }
+                for(int i = 0; i < m; i++){
+                    if(person[i][0] == 0 && person[i][1] == car.x && person[i][2] == car.y){
 
-
-    public static void findTheNearestMan(int x, int y) {
-        q1 = new LinkedList<>();
-        q1.add(new Node(x, y, 0));
-        visited1 = new boolean[n][n];
-        int cost = 0;
-        int min = Integer.MAX_VALUE;
-
-        while (!q1.isEmpty()) {
-            int qSize = q1.size();
-            for (int s = 0; s < qSize; s++) {
-                Node taxi = q1.poll();
-
-                if (map[taxi.x][taxi.y] == 2) {
-                    min = Math.min(cost, min);
-                    destX = taxi.x;
-                    destY = taxi.y;
-                    destNum = taxi.num;
-                    return;
-                }
-
-                for (int i = 0; i < 4; i++) {
-                    int dx = taxi.x + dir[i][0];
-                    int dy = taxi.y + dir[i][1];
-
-                    if (isRange(dx, dy) && !visited1[dx][dy] && map[dx][dy] != 1) {
-                        visited1[dx][dy] = true;
-                        q1.add(new Node(dx, dy, 0));
                     }
                 }
             }
 
-            cost++;
         }
 
-        fuel -= cost;
+
     }
 
 
-    public static void goDestination(int x, int y, int destNum) {
-        q2 = new LinkedList<>();
-        q2.add(new Node(destX, destY, destNum));
-        visited2 = new boolean[n][n];
-
-        int cost = 0;
-
-        while (!q2.isEmpty()) {
-            int qSize = q2.size();
-            for (int s = 0; s < qSize; s++) {
-                Node taxi = q2.poll();
-
-                if (taxi.num == destNum) {
-                    fuel += cost;
-                    flag = true;
-                    return;
-                }
-
-                for (int i = 0; i < 4; i++) {
-                    int dx = taxi.x + dir[i][0];
-                    int dy = taxi.y + dir[i][1];
-
-                    if (isRange(dx, dy) && !visited2[dx][dy] && map[dx][dy] != 1) {
-                        visited2[dx][dy] = true;
-                        q1.add(new Node(dx, dy, taxi.num));
-                    }
-                }
-            }
-
-            cost++;
-        }
-    }
 
     public static boolean isRange(int x, int y) {
         if (x >= 0 && y >= 0 && x < n && y < m) {

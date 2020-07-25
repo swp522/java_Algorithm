2d7a76bcc9deb278cf48ee43bc56b008b9a2b7e4
swpark <swp522@naver.com>
2020-07-25 23:57:50 +0900

    [BAEKJOON] Re-Uploaded BOJ_2580_스도쿠. Finished not yet.

--- BAEKJOON/BOJ_2580_스도쿠.java
+++ BAEKJOON/BOJ_2580_스도쿠.java
@@ -16,6 +16,8 @@ import java.io.InputStreamReader;
 public class BOJ_2580_스도쿠 {
     public static void main(String[] args){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        solve();
 
 
     }

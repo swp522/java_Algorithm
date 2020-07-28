9dae291dbcaeb01269db5d28a434c7a0bcf9e0f4
swpark <swp522@naver.com>
2020-07-28 22:15:34 +0900

    [BAEKJOON] Upload BOJ_1941_소문난칠공주. Finished not yet.

---
+++ BAEKJOON/BOJ_1941_소문난칠공주.java
@@ -0,0 +1,26 @@
+// 2020_07_28_수_소문난칠공주
+
+/*
+ * 1. 소문난 칠공주를 결성할 수 있는 모든 경우의 수
+ *
+ * 2.
+ *
+ * 3.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1941_소문난칠공주 {
+
+    static char map[][];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        map = new char[5][5];
+
+    }
+}

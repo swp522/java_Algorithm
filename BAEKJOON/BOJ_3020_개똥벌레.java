d9f29916b4de16602792fe7d4da4ad83cf94a0a1
swpark <swp522@naver.com>
2020-08-22 22:48:57 +0900

    [BAEKJOON] Re-Uploaded BOJ_3020_개똥벌레. Finished not yet.

--- BAEKJOON/BOJ_3020_개똥벌레.java
+++ BAEKJOON/BOJ_3020_개똥벌레.java
@@ -22,8 +22,7 @@ public class BOJ_3020_개똥벌레 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        answer = 0;
+        answer = -1;
 
         solve();
         

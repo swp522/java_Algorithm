fed3db9855ef1ef25b3796d2aa234324c047bb84
swpark <swp522@naver.com>
2020-09-04 22:54:33 +0900

    [PROGRAMMERS] Uploaded PRO_2020KAKAO_자물쇠와열쇠. Finished not yet.

---
+++ PROGRAMMERS/PRO_2020KAKAO_자물쇠와열쇠.java
@@ -0,0 +1,34 @@
+// 2020_09_03_수_restart
+
+/*
+ * 1.
+ *
+ * 2.
+ *
+ * 3.
+ *
+ *
+ *
+ */
+
+public class PRO_2020KAKAO_자물쇠와열쇠 {
+    public static void main(String[] args) {
+        int[][] key = {{0, 0, 0},
+                       {1, 0, 0},
+                       {0, 1, 1}};
+        int[][] lock = {{1, 1, 1},
+                        {1, 1, 0},
+                        {1, 0, 1}};
+
+        System.out.println(solution(key, lock));
+    }
+
+    public static boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+
+
+        return answer;
+    }
+
+
+}

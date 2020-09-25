ee8eb435128ac07bece7a3df4657363a3ffe7d0b
swpark <swp522@naver.com>
2020-09-25 22:11:38 +0900

    [BAEKJOON] Re-Uploaded BOJ_1806_부분합.

--- BAEKJOON/BOJ_1806_부분합.java
+++ BAEKJOON/BOJ_1806_부분합.java
@@ -1,11 +1,16 @@
-// 2020_07_01_수_부분합
+// 2020_09_25_금_부분합
 
 /*
  * 1. 부분합이 S 이상이 되는 가장 짧은 길이 구하기
  *
  * 2. 10 ≤ N < 100,000  , 0 < S ≤ 100,000,000
  *
- * 3. 투포인터로 구현 -> 시간 복잡도 O(2N)
+ * 3. 투포인터로
+ *
+ *    1). first, second 포인터 이동하면서 min 값 구하기
+ *
+ *    2).길이가 100000 이상이면 최소 길이 구하는게 불가능함
+ *
  */
 
 import java.io.BufferedReader;
@@ -28,25 +33,26 @@ public class BOJ_1806_부분합 {
 
         int minLength = 100001;
         int sum = 0;
-        int rightPointer = 0;
-        int leftPointer = 0;
+        int first = 0;
+        int second = 0;
 
         while (true) {
             if (sum >= s) {
-                sum -= arr[leftPointer++];
-                minLength = Math.min(minLength, (rightPointer - leftPointer) + 1);
-            } else if (rightPointer == n) {
+                sum -= arr[second++];
+                minLength = Math.min(minLength, (first - second) + 1);
+            } else if (first == n) {
                 break;
             } else {
-                sum += arr[rightPointer++];
+                sum += arr[first++];
             }
         }
 
-        if (minLength == 100001) {
+        if (minLength > 100000){
             System.out.println(0);
-        } else {
+        } else{
             System.out.println(minLength);
         }
 
     }
+
 }

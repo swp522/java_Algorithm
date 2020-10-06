32aa73d147b2863a3d01d1994fac2c9d6dd00837
swpark <swp522@naver.com>
2020-10-06 23:27:19 +0900

    [BAEKJOON] Uploaded BOJ_2208_보석줍기. Finished not yet.

---
+++ BAEKJOON/BOJ_2208_보석줍기.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2208_보석줍기 {
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}

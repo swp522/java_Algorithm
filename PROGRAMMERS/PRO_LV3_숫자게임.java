e288ea63b737acd464db44906025440e8acb3a0b
swpark <swp522@naver.com>
2020-10-21 11:27:43 +0900

    [PROGRAMMERS] Uploaded PRO_LV3_숫자게임

---
+++ PROGRAMMERS/PRO_LV3_숫자게임.java
@@ -0,0 +1,44 @@
+// 2020_10_21_수
+
+/*
+
+    1. B팀이 얻는 최대 승점
+
+    2. 1 <= A B 길이 <= 100,000   /   1 <= A B 값 <= 1,000,000,000
+
+    3. 1) 처음에 A 배열은 고정으로 두는 거라고 생각했음
+
+       2) 정렬하고 뒤에서부터 비교하는데 B값이 A보다 크면 answer++하고 B의 index--
+
+       3) 안크면 B의 값을 비교에 쓸 필요가 없으니까 continue 처리
+
+ */
+
+import java.util.Arrays;
+
+public class PRO_LV3_숫자게임 {
+    public static void main(String[] args) {
+        int[] A = {5, 1, 3, 7};
+        int[] B = {2, 2, 6, 8};
+
+        System.out.println(solution(A, B));
+    }
+
+    public static int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int index = B.length - 1;
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (A[i] < B[index]) {
+                answer++;
+                index--;
+            } else{
+                continue;
+            }
+        }
+
+        return answer;
+    }
+}

86f02c0b860a0b0543a76af0b2313993684e3255
swpark <swp522@naver.com>
2020-11-10 17:26:01 +0900

    [BAEKJOON] Finished BOJ_1715_카드정렬하기

--- BAEKJOON/BOJ_1715_카드정렬하기.java
+++ BAEKJOON/BOJ_1715_카드정렬하기.java
@@ -1,5 +1,39 @@
-public class BOJ_1715_카드정렬하기 {
-    public static void main(String[] args){
+// 2020_11_10_화
 
+/*
+
+    1. 최소 비교 횟수 구하기
+
+    2. 1 ≤ N ≤ 100,000
+
+    3. 우선순위 큐로 구현
+
+       넣는 순서에 상관없이 정수값 작은거 순서대로 poll 함
+
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_1715_카드정렬하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        int answer = 0;
+        while(pq.size() > 1){
+            int first = pq.remove();
+            int second = pq.remove();
+            answer += (first + second);
+            pq.add(first + second);
+        }
+
+        System.out.println(answer);
     }
 }

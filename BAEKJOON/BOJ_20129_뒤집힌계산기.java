90711ee456950826d07d2d5e7bf847e7e7a54ee1
swpark <swp522@naver.com>
2020-11-20 17:38:42 +0900

    [BAEKJOON] Uploaded BOJ_20129_뒤집힌계산기. Finished not yet.

---
+++ BAEKJOON/BOJ_20129_뒤집힌계산기.java
@@ -0,0 +1,19 @@
+// 2020_11_20_금
+
+/*
+
+    1. 식 계산하기
+
+    2. 1 ≤ pi ≤ 4 (1 ≤ i ≤ 4)   /   pi ≠ pj  / p 값은 각각 + - * / 우선순위
+
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_20129_뒤집힌계산기 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}

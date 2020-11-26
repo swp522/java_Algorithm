c09583495ed61791ec22093f5338f62b00c46668
swpark <swp522@naver.com>
2020-11-26 21:48:02 +0900

    [BAEKJOON] Uploaded BOJ_2841_외계인의기타연주. Finished not yet.

---
+++ BAEKJOON/BOJ_2841_외계인의기타연주.java
@@ -0,0 +1,12 @@
+// 2020_11_26_목
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2841_외계인의기타연주 {
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}

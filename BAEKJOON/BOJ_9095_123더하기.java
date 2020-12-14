4bac023c578b0786a1a4ff8535cfbd3e126d8a85
swpark <swp522@naver.com>
2020-12-14 16:21:32 +0900

    [BAEKJOON] Uploaded BOJ_9095_123더하기.java. Finished not yet.

---
+++ BAEKJOON/BOJ_9095_123더하기.java
@@ -0,0 +1,26 @@
+// 2020_12_14_월
+
+/*
+
+    1. 정수 n을 1, 2, 3의 합으로 나타내는 방법의 수 구하기
+
+    2. n < 11
+
+    3. 구현
+
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_9095_123더하기 {
+    public static void main(String[] arsg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+
+        }
+
+    }
+}

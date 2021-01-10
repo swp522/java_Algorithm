7dcfada79abb21e7dc5a54dd5e18a29393267bc9
swpark <swp522@naver.com>
2021-01-10 22:52:04 +0900

    [BAEKJOON] Re-Uploaded BOJ_2210_숫자판점프.java. Finished not yet.

--- BAEKJOON/BOJ_2210_숫자판점프.java
+++ BAEKJOON/BOJ_2210_숫자판점프.java
@@ -1,10 +1,10 @@
-// 2021_01_09_토
+// 2021_01_10_일
 // version 1.0
 
 
 public class BOJ_2210_숫자판점프 {
     public static void main(String[] args){
-        BufferedReader br = new BufferedReader();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
     }
 }

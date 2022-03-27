5bad90b02b4fb68599f2e0979c3427f837afc461
swpark <dch5222@gmail.com>
2022-03-27 22:38:25 +0900

    [BAEKJOON]Re-Uploaded BOJ_18310_안테나.java

--- BAEKJOON/BOJ_18310_안테나.java
+++ BAEKJOON/BOJ_18310_안테나.java
@@ -1,5 +1,11 @@
-// 2020_04_03_금_과제_23회차_안테나
-// 1 3 5 9 9 9 9 인 경우 테케에서 누락된 듯
+/*
+1. 구현
+
+2. O(n)
+
+3. 중점을 찾아주면 되는 구현 문제
+
+ */
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,26 +14,22 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_18310_안테나 {
-
-    static int n;
-    static int[] houses;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        houses = new int[n + 1];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            houses[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(houses);
+        Arrays.sort(arr);
 
-        if (n % 2 == 0) {
-            System.out.println(houses[(n / 2)]);
-        } else {
-            System.out.println(houses[n / 2] + 1);
+        if(n % 2 == 1){
+            System.out.println(arr[n / 2]);
+        } else{
+            System.out.println(arr[(n / 2) - 1]);
         }
     }
 }

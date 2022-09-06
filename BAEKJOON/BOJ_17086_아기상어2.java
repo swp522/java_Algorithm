37f3a1fdf280f9c238f0852e871a39ec71a3711d
swpark <dch5222@gmail.com>
2022-09-06 23:18:05 +0900

    [BAEKJOON] Finished not yet.]

---
+++ BAEKJOON/BOJ_17086_아기상어2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_17086_아기상어2 {
+
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+
+    }
+}

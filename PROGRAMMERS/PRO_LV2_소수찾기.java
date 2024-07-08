da7610a12c787cade64c760d16df49dc775a675f
swpark <dch5222@gmail.com>
2024-07-08 23:08:10 +0900

    [PRO] Uploaded PRO_LV2_소수찾기.java

---
+++ PROGRAMMERS/PRO_LV2_소수찾기.java
@@ -0,0 +1,19 @@
+/* 2024.07.08 (월) PRO_LV2_소수찾기
+ * DFS, 백트래킹 구현
+ *
+ */
+public class PRO_LV2_소수찾기 {
+    public static void main(String[] args){
+        String numbers = "011";
+
+        System.out.println(solution(numbers));
+    }
+
+    public static int solution(String numbers) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
